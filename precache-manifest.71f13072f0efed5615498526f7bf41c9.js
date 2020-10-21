8fc4ed0d0498ee08c46316487392f3466b1f6017
Asapanna Rakesh <rakeshark22@gmail.com>
2020-10-21 20:01:19 +0530

    Updates

---
+++ precache-manifest.71f13072f0efed5615498526f7bf41c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81a9c6fae0affb7a966a8680b8fbf88d",
+    "url": "/qdraw/index.html"
+  },
+  {
+    "revision": "63049691663700b1a1d6",
+    "url": "/qdraw/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "a4fdb008fd5b33f457b9",
+    "url": "/qdraw/static/js/2.0ce8f115.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/qdraw/static/js/2.0ce8f115.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63049691663700b1a1d6",
+    "url": "/qdraw/static/js/main.27beb75e.chunk.js"
+  },
+  {
+    "revision": "8c459c30f6d0f6787c97",
+    "url": "/qdraw/static/js/runtime-main.5106c99c.js"
+  }
+]);
