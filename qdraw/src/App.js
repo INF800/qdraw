615fd5b8176d2c8138e3736db7e284b0b2b9b5f0
Asapanna Rakesh <rakeshark22@gmail.com>
2020-10-21 17:22:43 +0530

    initial commit

---
+++ qdraw/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import SketchPad from './sketch/SketchPad'
+
+function App() {
+
+  return (
+    <div className="App">
+      <SketchPad />
+    </div>
+  )
+}
+
+
+export default App;
